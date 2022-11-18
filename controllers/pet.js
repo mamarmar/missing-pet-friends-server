8222b574ad5319b53d55096b841bd3992c7a9182
Margarita Marmaridou <margmar@protonmail.com>
2022-11-18 16:45:05 +0200

    pet controllers completed

---
+++ controllers/pet.js
@@ -0,0 +1,34 @@
+import petModel from "../models/petModel";
+
+//Get all missing pets
+export const getAllPets = async (req, res) => {
+  try {
+    const pets = await petModel.find({});
+    res.status(201).send(pets);
+  } catch (err) {
+    res.status(404).send(err);
+  }
+};
+
+//Get all pets lost at specific city
+//City is sent as a parameter of the query object of the request object
+export const getFilteredPets = async (req, res) => {
+  try {
+    const { city } = req.query;
+    const pets = await petModel.find({ city });
+    res.status(201).send(pets);
+  } catch (err) {
+    res.status(404).send(err);
+  }
+};
+
+//Create new lost pet listing
+export const createPetListing = async (req, res) => {
+  const newPet = new petModel(req.body);
+  try {
+    await newPet.save();
+    res.status(201).send(newPet);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+};
